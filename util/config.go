c13a2632b9b480ccc18a531bf5875d1e20a9370b
tonisco <tonyumeozulu@gmail.com>
2023-09-19 14:22:28 +0100

    created sessions table

--- util/config.go
+++ util/config.go
@@ -9,11 +9,12 @@ import (
 // config stores all configurations of the application.
 // the values are read by viper from a config file or environment variable
 type Config struct {
-	DBDriver            string        `mapstructure:"DB_DRIVER"`
-	DBSource            string        `mapstructure:"DB_SOURCE"`
-	ServerAddress       string        `mapstructure:"SERVER_ADDRESS"`
-	TokenSymmetricKey   string        `mapstructure:"TOKEN_SYMMETRIC_KEY=12345678901234567890123456789012"`
-	AccessTokenDuration time.Duration `mapstructure:"ACCESS_TOKEN_DURATION=15m"`
+	DBDriver             string        `mapstructure:"DB_DRIVER"`
+	DBSource             string        `mapstructure:"DB_SOURCE"`
+	ServerAddress        string        `mapstructure:"SERVER_ADDRESS"`
+	TokenSymmetricKey    string        `mapstructure:"TOKEN_SYMMETRIC_KEY=12345678901234567890123456789012"`
+	AccessTokenDuration  time.Duration `mapstructure:"ACCESS_TOKEN_DURATION=15m"`
+	RefreshTokenDuration time.Duration `mapstructure:"REFRESH_TOKEN_DURATION=15m"`
 }
 
 // LoadConfig reads configuration from the config file or environment variable
